634accd44b4867b3415fea72979e7a4abecd8534
Wush Wu <wush978@gmail.com>
2013-05-04 23:15:42 +0800

    bug during add type

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ get_path <- function(name, ...) {
   root_path <- "記帳App"
   normalizePath(sprintf("%s/%s", root_path, name), ...)
 }
-extra_label <- c("其他", "無")
+extra_label <- c("其他(新增)", "無")
 
 list_account <- function(id, label) {
   account <- read.csv(get_path("account.csv", mustWork=TRUE), stringsAsFactors=FALSE)
@@ -30,8 +30,13 @@ shinyServer(function(input, output) {
   })
   output[["result"]] <- renderUI({
     value <- as.integer(input$value)
-    browser()
-    if (is.na(value)) return(tableOutput("history")) else return(tableOutput("insert"))
+    if (is.na(value)) {
+      print("history")
+      return(tableOutput("history"))
+    } else {
+      print("insert")
+      return(tableOutput("insert"))
+    } 
   })
   output[["history"]] <- renderTable({
     query_date <- tryCatch({
@@ -49,6 +54,7 @@ shinyServer(function(input, output) {
     return(retval)
   })
   output[["insert"]] <- renderTable({
+    browser()
     query_date <- tryCatch({
       as.Date(input$date)
     }, error = function(e) {
@@ -57,6 +63,13 @@ shinyServer(function(input, output) {
     if (is.na(query_date)) stop("日期格式必須為yyyy-mm-dd")
     file_name <- get_path(format(query_date, "%Y-%m.csv"), mustWork=FALSE)
     retval <- list("日期"=format(query_date), "類別"=input$type, "金額"=as.integer(input$value), "輸出帳戶"=input[["out-account"]], "輸入帳戶" = input[["in-account"]], "手續費" = input$fee, "其他備注" = input$remark)
+    browser()
+    if (input$type == extra_label[1]) { # add type
+      if (input[["type-extra"]] == "") stop("請於「類別-備注」欄位填寫新增的類別名稱")
+      src <- read.csv(get_path("type.csv", mustWork=TRUE), stringsAsFactors=FALSE)
+      src$Name <- c(src$Name , input[["type-extra"]])
+      write.csv(src, file=get_path("type.csv", mustWork=TRUE), row.names=FALSE)
+    }
     retval <- data.frame(retval, check.names=FALSE, stringsAsFactors=FALSE)
     if (file.exists(file_name)) {
       retval <- rbind(retval, read.csv(file=file_name, stringsAsFactors=FALSE))
